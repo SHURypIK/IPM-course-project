986322efda34a7277dc9b115b277068f5df4f929
SHURypIK <147335447+SHURypIK@users.noreply.github.com>
2024-10-27 21:31:45 +0300

    create servere

---
+++ Server/IPM-Coures-Project/src/main/java/com/example/IPM/Coures/Project/IpmCouresProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.IPM.Coures.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpmCouresProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpmCouresProjectApplication.class, args);
+	}
+
+}
